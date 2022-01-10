b2908d1d6a89f17906d69f8315d1f24047e446e8
ruukichi <ngovietlc93@gmail.com>
2022-01-11 00:09:52 +0700

    xay dung lop chi ghi trong class

---
+++ src/com/codegym/Main.java
@@ -0,0 +1,12 @@
+package com.codegym;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Student student1 = new Student();
+        Student student2 = new Student();
+        student2.setName("Lee");
+        System.out.println(student1.getName());
+        System.out.println(student2.getName());
+    }
+}
